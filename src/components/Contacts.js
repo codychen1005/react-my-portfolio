f429c9a69985a28a72c152f885ff19248efadcc2
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-17 20:02:17 -0500

    1

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,6 +3,58 @@ import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { TextField, Typography, Button, Grid, Box } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import Navbar from "./Navbar";
+import emailjs from "emailjs-com";
+
+// export default function ContactUs() {
+
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs
+    .sendForm(
+      "gmail",
+      "template_prqnt4j",
+      e.target,
+      "user_GX8OtLy7CpXrE2FI3Hexf"
+    )
+    .then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  e.target.reset();
+}
+
+//   return(
+//     <div>
+//      <Navbar />
+//         <div className="container">
+//         <form onSubmit={sendEmail}>
+//                 <div className="row pt-5 mx-auto">
+//                     <div className="col-8 form-group mx-auto">
+//                         <input type="text" className="form-control" placeholder="Name" name="name"/>
+//                     </div>
+//                     <div className="col-8 form-group pt-2 mx-auto">
+//                         <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+//                     </div>
+//                     <div className="col-8 form-group pt-2 mx-auto">
+//                         <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+//                     </div>
+//                     <div className="col-8 form-group pt-2 mx-auto">
+//                         <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+//                     </div>
+//                     <div className="col-8 pt-3 mx-auto">
+//                         <input type="submit" className="btn btn-info" value="Send Message"></input>
+//                     </div>
+//                 </div>
+//             </form>
+//         </div>
+//     </div>
+// )
+// }
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -10,7 +62,7 @@ const useStyles = makeStyles((theme) => ({
     left: "50%",
     transform: "translate(-50%,-50%)",
     position: "absolute",
-    width: "500px"
+    width: "500px",
   },
   button: {
     marginTop: "1rem",
@@ -85,10 +137,9 @@ const Contacts = () => {
             fullWidth={true}
             label="Leave your message here"
             variant="outlined"
-            inputProps={{ style: { color: "white", height:100}  }}
+            inputProps={{ style: { color: "white", height: 100 } }}
             margin="dense"
             size="medium"
-            
           />
           <br />
           <Button
